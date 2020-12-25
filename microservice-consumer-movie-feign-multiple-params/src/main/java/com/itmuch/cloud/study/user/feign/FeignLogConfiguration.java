4d0ec4007652095ac819e5f708e4ddfbdbe2f481
weiwei10 <weiwei10@hikvision.com>
2020-12-25 10:06:59 +0800

    springcloud实践代码

---
+++ microservice-consumer-movie-feign-multiple-params/src/main/java/com/itmuch/cloud/study/user/feign/FeignLogConfiguration.java
@@ -0,0 +1,19 @@
+package com.itmuch.cloud.study.user.feign;
+
+import feign.Logger;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class FeignLogConfiguration {
+  @Bean
+  Logger.Level feignLoggerLevel() {
+    /**
+     * NONE 不记录日志
+     * BASIC  仅记录请求方法URL，相应状态码以及执行时间
+     * HEADERS  记录BASIC基础上，记录请求和相应的header
+     * FULL  记录请求和响应的header body 和元数据
+     * **/
+    return Logger.Level.FULL;
+  }
+}
